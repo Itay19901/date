2d79d7977ad0fbaf2c7502b26b23a7e9c42e2aaf
itay.d <itaydaushin59@gmail.com>
2023-04-09 18:57:29 +0300

    full commit

--- date_listener.py
+++ date_listener.py
@@ -6,6 +6,7 @@ curr_date = datetime.date.today()
 # Get the title of the day from the current date
 day_of_week = curr_date.strftime("%A")
 
+
 def get_file(conn, filename, replacements):
     with open(filename, 'r') as file:
         content = file.read()
@@ -17,6 +18,7 @@ def get_file(conn, filename, replacements):
         # For tests uncomment next line
         # print(content)
 
+
 # Run the socket on port 8080 on localhost
 def main():
     host = ''
@@ -27,8 +29,6 @@ def main():
     s.listen(1)
 
     print(f"Listening on port {port}...")
-
-
     while True:
         conn, addr = s.accept()
         print(f"Connection from {addr}")
@@ -38,6 +38,7 @@ def main():
         get_file(conn, filename, replacements)
 
         conn.close()
+
 
 if __name__ == '__main__':
     main()
---
+++ readme.txt
@@ -0,0 +1,4 @@
+$ docker build -t date .
+after image is created run
+
+$ docker run -p 8080:8080 --rm date
